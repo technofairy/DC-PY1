49799877751d42935739e1006350f1ee3f288bdd
technofairy <115464603+technofairy@users.noreply.github.com>
2022-10-22 14:35:47 +0300

    Лабораторная_работа_3_Попытка_2

---
+++ Лабораторная_работа_3/task_1.py
@@ -0,0 +1,8 @@
+src = not False and True or False and not True
+
+# not False and True or False and False
+# True and True or False and False
+# True or False
+
+result = True
+print(src == result)
---
+++ Лабораторная_работа_3/task_9.py
@@ -0,0 +1,12 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+for money in range(1, months + 1):    # или for i in range(months)
+    money = (spend - salary)
+    spend = spend * (increase + 1)
+    need_money += money
+
+print(round(need_money))
